1c5287edde570536baa79b9be5fb8d397711a88e
Alonso <70250204+Alonsovn@users.noreply.github.com>
2022-03-14 22:50:10 -0600

    Add files via upload

---
+++ main.py
@@ -0,0 +1,45 @@
+from flask import Flask, render_template
+from flask.views import MethodView
+from wtforms import Form, StringField, SubmitField
+
+app = Flask(__name__)
+
+
+class HomePage(MethodView):
+
+    def get(self):
+        return render_template('index.html')
+
+
+class BillFormPage(MethodView):
+
+    def get(self):
+        bill_form = BillForm()
+        return render_template('bill_form_page.html',
+                               billform=bill_form)
+
+
+class ResultsPage(MethodView):
+    pass
+
+
+class BillForm(Form):
+    amount = StringField("Bill Amount: ")
+    period = StringField("Bill Period: ")
+
+    name1 = StringField("Name: ")
+    days_in_house1 = StringField("Days in the house: ")
+
+    name2 = StringField("Name: ")
+    days_in_house2 = StringField("Days in the house: ")
+
+    button = SubmitField("Calculate")
+
+
+
+
+app.add_url_rule('/', view_func=HomePage.as_view('home_page'))
+app.add_url_rule('/bill', view_func=BillFormPage.as_view('bill_form_page'))
+
+app.run()
+# app.run(debug=True)
